0f0d7d03ca74501a2bc4c5493dbceabd18ca5cbe
barifche <rifbache@gmail.com>
2020-04-09 21:09:27 +0200

    les trois exercices validés 08, 07.c, 07.b

---
+++ exo_08.rb
@@ -0,0 +1,9 @@
+puts " Quel ets votre prénom ? "
+print " > "
+a = gets.chomp
+puts " Quel est votre nom ? "
+print " > "
+
+b= gets.chomp
+
+puts " Bonjour #{a} #{b}! Te connaissant, je sais que t'es un mec sympa "
