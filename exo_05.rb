4a8f21e37d6737e2cdee98496f611a7fe767ff7a
barifche <rifbache@gmail.com>
2020-04-09 19:40:57 +0200

    exercice 05 validé

---
+++ exo_05.rb
@@ -0,0 +1,29 @@
+puts " On va compter le nombre d'heures de travail à THP"
+
+puts " Travail : #{10 * 5 * 11} "
+puts "En minute ça fait : #{10 * 5 * 11 * 60} "
+# (#{}) fait en sorte que le contenu ne soit pas un string : genre une conversion de ce qui est écrit dans les accollades.
+puts " Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60
+# fait l'opération et nous renvoie le nombre de seconde auxquelles on a travaillé avec THP
+
+puts " Est-ce que c'est que 3 + 2 < 5 -7 ?"
+
+puts 3 + 2 < 5 -7
+
+puts " ça fait combien 3 + 2 ? #{3 + 2} "
+
+puts " ça fait combien 5 - 7 ? #{5 - 7} "
+
+puts " OK , c'est faux alors! "
+
+puts " c'est drole ça , faisons -en plus :"
+
+puts " Est ce que 5 est plus grand que -2 ? #{5 > -2} "
+# renvoi un type Booleen: True or false... c'est true
+puts " Est ce que 5 est superieur ou égale à -2 ? #{5>= -2} "
+# true
+
+puts " Est ce que 5 est inferieur ou égale à -2 ? #{5 <= -2}"
+# renvoie : false
